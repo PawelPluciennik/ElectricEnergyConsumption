d14ff24f959f0efc9a0f035ec7cd9544c123aaba
PawelPluciennik <pawciopl62@gmail.com>
2016-10-20 20:03:36 +0200

    First change
    Program wyswietla Hello world

--- ElectricEnergyConsumption/Program.cs
+++ ElectricEnergyConsumption/Program.cs
@@ -10,6 +10,8 @@ namespace ElectricEnergyConsumption
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
         }
     }
 }
